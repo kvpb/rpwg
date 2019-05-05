af860bd9e5b3d70b63e5de917e6d4af2e5d2de0c
Karl Bertin <local-part@domain>
2019-05-05 21:04:04 +0200

    RPwG.rb updated to version 1.10: added output color.

--- RPwG.rb
+++ RPwG.rb
@@ -1,24 +1,25 @@
 #!/usr/bin/env ruby
 
 character_set = [ "!", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~" ] # ASCII order, no 032, """, "\" or "#".
-bad_character_set = [ "!", "$", "%", "&", "'", "(", ")", "*", ",", "-", ".", "/", ":", ";", "?", "@", "A", "M", "Q", "W", "Z", "[", "]", "^", "`", "a", "m", "q", "w", "z", "{", "|", "}", "~" ]
+qwazerty_character_set = [ "!", "$", "%", "&", "'", "(", ")", "*", ",", "-", ".", "/", ":", ";", "?", "@", "A", "M", "Q", "W", "Z", "[", "]", "^", "`", "a", "m", "q", "w", "z", "{", "|", "}", "~" ]
 
-puts """
+puts `tput setaf 1` + """
 RANDOM      PASSWORD     GENERATOR
    ,-----, ,-----,         ,-----,
   / /'/ / / / / / ,-,-,-, / /''-'
  / / | | / ,---' / / / / / /_/'/
 '-'  '-''-'     '-----' '-----'
-V  E  R  S  I  O  N     1  .  0  1
-
-Enter the password's length (in arabic numbers)."""
+V  E  R  S  I  O  N     1  .  1  0""" + `tput sgr0` + "\n" * 2
+print `tput setaf 1` + "Password length " + `tput smul` + "(digits only)" + `tput sgr0` + `tput setaf 1` + ":" + `tput sgr0` + " " + `tput setaf 3`
 password_length = gets.to_i
+print `tput sgr0`
 
-puts "\nShall the password be qwazerty sensitive (\"yes\" or \"no\") ?"
-password_type = gets
+print `tput setaf 1` + "QWAZERTY sensitivity " + `tput smul` + "('y' for yes)" + `tput sgr0` + `tput setaf 1` + "?" + `tput sgr0` + " " + `tput setaf 3`
+qwazerty_sensitivity = gets
+print `tput sgr0`
 
-if password_type =~ /(|^)yes(|$)/i
-	character_set = character_set - bad_character_set
+if qwazerty_sensitivity =~ /(|^)y(|$)/i
+	character_set = character_set - qwazerty_character_set
 else
 	end
 
